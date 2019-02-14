52930f71c7b0629a4779d7c24c2a6c7ef0db8c6d
Ari Utomo <robert.unix98@gmail.com>
2019-02-14 13:35:58 +0700

    First Commit

---
+++ AgenCerdas.java
@@ -0,0 +1,51 @@
+public class AgenCerdas {
+    private String lingkungan[][];
+    AgenCerdas(int size){
+        lingkungan = new String[size][3];
+    }
+    public void setKondisi(int i, String Nama,String
+            location,String status){
+        lingkungan[i-1][0]=Nama;
+        lingkungan[i-1][1]=location;
+        lingkungan[i-1][2]=status;
+        System.out.println();
+    }
+    public void vacumm (int i,String location, String status){
+        if ("kotor".equals(status.toLowerCase())){
+            System.out.println("----bersih-bersih----");
+            status = "bersih";
+            lingkungan[i][2]=status;
+            vacumm(i,location,status);
+        } else {
+            System.out.println(lingkungan[i][0]+" sudah bersih");
+            if ("kiri".equals(location.toLowerCase())) {
+                System.out.println("geser kanan-->");
+            } else if ("kanan".equals(location.toLowerCase())) {
+                System.out.println("<--geser kiri");
+            } else if ("atas".equals(location.toLowerCase())){
+                System.out.println("geser bawah");
+            } else if ("bawah".equals(location.toLowerCase())) {
+                System.out.println("geser atas");
+            }
+            System.out.println();
+        }
+    }
+    public void cleaning(){
+        for (int i = 0; i<lingkungan.length; i++){
+            System.out.println(lingkungan[i][0]);
+            vacumm(i,lingkungan[i][1],lingkungan[i][2]);
+        }
+        System.out.println("SEMUA SUDAH BERSIH");
+    }
+    public static void main(String[] args) {
+        AgenCerdas a = new AgenCerdas(6);
+        a.setKondisi(1, "Lokasi 1", "kanan", "bersih");
+        a.setKondisi(2, "Lokasi 2", "atas", "kotor");
+        a.setKondisi(3, "Lokasi 3", "kiri", "kotor");
+        a.setKondisi(4, "Lokasi 4", "atas", "kotor");
+        a.setKondisi(5, "Lokasi 5", "kanan", "kotor");
+        a.setKondisi(6, "Lokasi 6", "final", "bersih");
+        a.cleaning();
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# AgenCerdasBAB1
+# KB-AgenCerdasBAB1
+Tugas Praktikum Kecerdasan Buatan
+
+Kelompok 1 
+- Afgani Fajar Rizky
+- Bayu Reza Gunawan
+- Gusti Rahman Pariska Ajitama
+- Muhammad Yusuf Azari
+- Robertus Dwi Ari Utomo
